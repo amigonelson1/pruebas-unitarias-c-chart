9447e1c67894df57f1e9eb090648bb46bc7f6af6
Ricardo Jose Cedeño Mancilla <sjaimes16@outlook.com>
2024-07-18 16:26:15 -0500

    commit

---
+++ Pruebas.Business/Repositories/IPersonaRepository.cs
@@ -0,0 +1,15 @@
+﻿using Pruebas.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas.Business.Repositories
+{
+  public interface IPersonaRepository
+  {
+    public ResponseDto<List<Persona>> GetPersonas();
+    public ResponseDto<int> AddPersona(Persona persona);
+  }
+}
---
+++ Pruebas.Core/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas.Core
+{
+  public class Persona
+  {
+    public string Id { get; set; } = string.Empty;
+    public string Nombre { get; set; } = string.Empty;
+    public int edad = 0;
+  }
+}
